6ad4bd4ebdc3ac991a2bf2f685b9cc2092a8d79a
norbertastier <astier.norbert@gmail.com>
2023-07-30 22:37:22 +0200

    Third lecture

---
+++ p3.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+inputs = [1, 2, 3, 2.5]
+
+weights = [[0.2, 0.8, -0.5, 1],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+print("Weight size : {}".format(np.shape(weights)))
+print("Input size : {}".format(np.shape(inputs)))
+
+output = np.dot(weights, inputs) + biases
+print(output)
+
