1badffc22d4b01288f6a558beebc0495e61855c6
norbertastier <astier.norbert@gmail.com>
2023-08-01 23:28:37 +0200

    Sixth lecture

--- p6.py
+++ p6.py
@@ -10,7 +10,7 @@ X = [[1.0, 2.0, 3.0, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
-X, y = spiral_data(100, 3)
+X, y = spiral_data(samples=100, classes=3)
 
 
 class Layer_Dense:
@@ -24,10 +24,24 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0,inputs)
 
-layer1 = Layer_Dense(2,5)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims= True)
+        self.output = probabilities
+
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
